c20b5c4ef8d72fb0c46566cf33904526e76681d9
Alessandro Vecchi <avecchi.53@gmail.com>
2021-07-12 15:06:32 -0600

    feat: split grib chunks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export const sum = (a: number, b: number) => {
-  return a + b
+export const readGrib = (data: Buffer) => {
+  const gribs = splitGribs(data)
+
+  return gribs
+}
+
+const splitGribs = (data: Buffer): Array<Buffer> => {
+  if (data.length === 0) return []
+
+  const gribCoded = data.slice(0, 4).toString()
+
+  if (gribCoded !== 'GRIB') throw new Error('Incorrectly Formatted GRIB')
+
+  const length = Number(data.slice(8, 16).readBigUInt64BE())
+
+  const gribData = data.slice(0, length)
+
+  return [gribData, ...splitGribs(data.slice(length))]
 }
