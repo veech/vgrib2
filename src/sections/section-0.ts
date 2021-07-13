4ab8f93cf85d64fcec82b688baa71935ac09f678
Alessandro Vecchi <avecchi.53@gmail.com>
2021-07-13 10:44:57 -0600

    refactor: return section 0 data in data property

--- src/sections/section-0.ts
+++ src/sections/section-0.ts
@@ -3,11 +3,20 @@
  *
  * [Read more...](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_sect0.shtml)
  */
-export const parseSection0 = (section: Buffer) => ({
-  /** Discipline [Table 0.0](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table0-0.shtml) */
-  discipline: section.readUInt8(6),
-  /** Edition number - 2 for GRIB2 */
-  gribEdition: section.readUInt8(7),
-  /** Total length of GRIB message in octets (All sections) */
-  gribLength: Number(section.readBigUInt64BE(8))
-})
+export const parseSection0 = (section: Buffer) => {
+  return {
+    /** Number of GRIB section */
+    sectionNumber: 0,
+    /** Length of GRIB section (Always 16 for Section 0)*/
+    length: 16,
+    /** Section 0 Data */
+    data: {
+      /** Discipline [Table 0.0](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table0-0.shtml) */
+      discipline: section.readUInt8(6),
+      /** Edition number - 2 for GRIB2 */
+      gribEdition: section.readUInt8(7),
+      /** Total length of GRIB message in octets (All sections) */
+      gribLength: Number(section.readBigUInt64BE(8))
+    }
+  }
+}
