a18259fab40977d2b6366ad728f32994155d9c00
Alessandro Vecchi <avecchi.53@gmail.com>
2021-07-13 10:38:53 -0600

    chore: add comments to section 0

--- src/sections/section-0.ts
+++ src/sections/section-0.ts
@@ -4,7 +4,10 @@
  * [Read more...](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_sect0.shtml)
  */
 export const parseSection0 = (section: Buffer) => ({
+  /** Discipline [Table 0.0](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table0-0.shtml) */
   discipline: section.readUInt8(6),
+  /** Edition number - 2 for GRIB2 */
   gribEdition: section.readUInt8(7),
+  /** Total length of GRIB message in octets (All sections) */
   gribLength: Number(section.readBigUInt64BE(8))
 })
